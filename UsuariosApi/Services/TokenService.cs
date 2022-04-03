0b43c81e156a212b299f1bbc28b899e70dd74641
Leandro-SI <llmanutencao@bol.com.br>
2022-04-03 16:12:49 -0300

    gerando token

---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitosUsuario = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("aisojcklc98372fnc12930fnhpansc93")
+            );
+
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddMinutes(2)
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new Token(tokenString);
+
+        }
+    }
+}
